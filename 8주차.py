d819f1be7f370fe2ac343c88f79903963987acdd
khs <69028828+happysang@users.noreply.github.com>
2021-10-15 04:37:33 +0900

    Update 8주차.py

--- 8주차.py
+++ 8주차.py
@@ -14,3 +14,30 @@ class Stack:
         item = self.last.item
         self.last = self.last.next
         return item
+
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.push(4)
+stack.push(5)
+
+for _ in range(5):
+    print(stack.pop())
+
+
+#문제1
+def isValid(s: str):
+    stack = []
+    table = {
+        ')':'(',
+        '}':'{',
+        ']':'['
+    }
+    
+    for char in s:
+        if char not in table:
+            stack.append(char)
+        elif not stack or table[char] != stack.pop():
+            return False
+    return len(stack) == 0

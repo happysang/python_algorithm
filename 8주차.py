62a694fe1ed86f3f630ed744dc2b6b4c506b760e
khs <69028828+happysang@users.noreply.github.com>
2021-10-17 05:37:27 +0900

    Update 8주차.py

--- 8주차.py
+++ 8주차.py
@@ -1,3 +1,6 @@
+from typing import AnyStr, List
+
+
 class Node:
     def __init__(self, item, next):
         self.item = item
@@ -41,3 +44,53 @@ def isValid(s: str):
         elif not stack or table[char] != stack.pop():
             return False
     return len(stack) == 0
+
+
+def q1(temperatures,num,res):
+    count = 0
+    for x in range(len(temperatures)):
+        if temperatures[0] >= temperatures[x]:
+            if x == len(temperatures)-1:
+                res.append(0)
+                temperatures.pop(0)
+                q1(temperatures, num+1, res)   
+            count += 1
+        else:
+            res.append(count)
+            temperatures.pop(0)
+            q1(temperatures, num+1, res)
+            
+        if len(temperatures) == 0:
+            return res
+# print(q1([73,74,75,71,69,72,76,73], 0, []))
+
+
+def q2(temperatures, res):
+    for x in range(len(temperatures)):
+        count = 0
+        for y in range(x, len(temperatures)):
+            if temperatures[x] >= temperatures[y]:
+                count += 1
+                if y == len(temperatures)-1:
+                    res.append(0)
+                continue
+            else:
+                res.append(count)
+                break
+    return res
+# print(q2([73,74,75,71,69,72,76,73], []))
+
+
+def dailyTemperature(temperatures:List[int]):
+    answer = [0]*len(temperatures)
+    stack = []
+    for i, cur in enumerate(temperatures):
+        #현재 온도가 스택 값보다 높다면 정답처리
+        print(answer, stack)
+        while stack and cur > temperatures[stack[-1]]:
+            last = stack.pop()
+            answer[last] = i - last
+        stack.append(i)
+    return answer
+
+print(dailyTemperature([73,74,75,71,69,72,76,73]))

ff555bc696113f819a49019e567fa0e1ea0f566f
khs <69028828+happysang@users.noreply.github.com>
2021-10-18 21:43:49 +0900

    Update 스택.py

--- 스택.py
+++ 스택.py
@@ -120,3 +120,24 @@ def removeDuplicateLetters2(s: str):
         
     return ''.join(stack)
 print(removeDuplicateLetters2("cbacdcbc"))
+
+
+import collections
+class Mystack:
+    def __init__(self):
+        self.q = collections.deque()
+
+    def push(self, x):
+        self.q.append(x)
+        #요소 삽입 후 맨 앞에 두는 상태로 재정렬
+        for _ in range(len(self.q)-1):
+            self.q.append(self.q.popleft())
+
+    def pop(self):
+        return self.q.popleft()
+
+    def top(self):
+        return self.q[0]
+
+    def empty(self):
+        return len(self.q) == 0
